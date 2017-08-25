b6b61127ab58bd0622e6c10561476fde3de373e3
fernand vargas <fernandvargas@fernands-MBP.lan>
2017-08-25 00:23:31 -0500

    add tag service to class TestServiceImpl and change file resource reference to db

--- src/main/java/com/local/test/service/impl/TestServiceImpl.java
+++ src/main/java/com/local/test/service/impl/TestServiceImpl.java
@@ -1,14 +1,16 @@
 package com.local.test.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.local.test.model.Test;
 import com.local.test.service.TestService;
 import com.local.test.util.Response;
 
+@Service
 public class TestServiceImpl implements TestService {
 
 	public Response saveTest(Test test) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Response(true, "ok");
 	}
 
 }
